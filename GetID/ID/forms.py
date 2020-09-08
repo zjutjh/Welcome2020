b7cb1e591c670ec0fb6bfc0d4416fa2b682da204
idevlab <idevlab@outlook.com>
2020-09-08 17:01:18 +0800

    improve ui

--- GetID/ID/forms.py
+++ GetID/ID/forms.py
@@ -1,10 +1,10 @@
 from django import forms
-from ID.front_validation import person_id_validator
+from .front_validation import person_id_validator
 
 
 class getIDForm(forms.Form):
     sname = forms.CharField()
-    sid = forms.CharField(validators=[person_id_validator])
+    sid = forms.CharField()
 
 
 class getDormForm(forms.Form):
--- GetID/ID/views.py
+++ GetID/ID/views.py
@@ -11,14 +11,20 @@ from enum import Enum
 imgType = ['pf_scenery', 'zh_scenery', 'pf_canteen', 'zh_canteen', 'pf_doom', 'zh_doom']
 img_folder = {'pf_scenery': '屏峰内景', 'zh_scenery': '朝晖内景', 'pf_canteen': '屏峰食堂',
               'zh_canteen': '朝晖食堂', 'pf_doom': '屏峰寝室', 'zh_doom': '朝晖寝室'}
-class res_msg(Enum):
-    MSG_ERROR="请正确填写信息"
-    MSG_NOT_FOUND="没有查到你的信息"
 
-def page_not_found(request,exception=None):
-    return render(request,'index.html')
-def page_error(request,exception=None):
-    return render(request,'index.html')
+
+class res_msg(Enum):
+    MSG_ERROR = "请正确填写信息"
+    MSG_NOT_FOUND = "没有查到你的信息"
+
+
+def page_not_found(request, exception=None):
+    return render(request, 'index.html')
+
+
+def page_error(request, exception=None):
+    return render(request, 'index.html')
+
 
 def img_show(request):
     img_type = request.GET.get('type')
@@ -27,14 +33,15 @@ def img_show(request):
     context = {'imgList': json.dumps(image_list), 'sname': 'sname'}
     return render(request, 'imgShow.html', context)
 
+
 def index_dorm(request):
     if request.method != "POST":
         return render(request, 'indexDorm.html')
-    
+
     uf = getDormForm(request.POST)
     if not uf.is_valid():
-        return render(request, 'indexDorm.html',{"message":res_msg.MSG_ERROR.value}) 
-    
+        return render(request, 'indexDorm.html', {"message": res_msg.MSG_ERROR.value})
+
     sname = uf.cleaned_data['sname']
     sid = uf.cleaned_data['sid']
     sha = hashlib.sha256()
@@ -42,35 +49,38 @@ def index_dorm(request):
     sid = sha.hexdigest()
     stu = Student.objects.filter(sname=sname, sid=sid)
     if not stu:
-        return render(request, 'indexDorm.html',{"message":res_msg.MSG_NOT_FOUND.value})
-    
+        return render(request, 'indexDorm.html', {"message": res_msg.MSG_NOT_FOUND.value})
+
     request.session['sname'] = sname
     stu = stu[0]
-    roommate = Student.objects.filter(sroom=stu.sroom,shouse=stu.shouse)
-    roommate=roommate.filter(~Q(sid=stu.sid))
+    roommate = Student.objects.filter(sroom=stu.sroom, shouse=stu.shouse)
+    roommate = roommate.filter(~Q(sid=stu.sid))
     context = {'sname': stu.sname, 'sroom': stu.sroom, 'scard': stu.scard, 'roommate': roommate,
-                'img_folder': img_folder,'sbed':stu.sbed,'shouse':stu.shouse}
+               'img_folder': img_folder, 'sbed': stu.sbed, 'shouse': stu.shouse}
     return render(request, 'getDorm.html', context)
-        
+
+
 def index(request):
     if request.method != "POST":
         return render(request, 'index.html', locals())
-    
+
     uf = getIDForm(request.POST)
+
     if not uf.is_valid():
         message = res_msg.MSG_ERROR.value
         return render(request, 'index.html', locals())
-    
+
     sname = uf.cleaned_data['sname']
     sid = uf.cleaned_data['sid']
     sha = hashlib.sha256()
     sha.update(sid.encode('utf8'))
     sid = sha.hexdigest()
+
     stu = Student.objects.filter(sname=sname, sid=sid)
-    if not stu :
+    if not stu:
         message = res_msg.MSG_NOT_FOUND.value
         return render(request, 'index.html', locals())
-    
+
     request.session['sname'] = sname
     stu = stu[0]
     roommate = Student.objects.filter(sroom=stu.sroom)
@@ -79,6 +89,5 @@ def index(request):
         obj = CampusImg.objects.filter(imgtype=img_type)
         img_list[v] = [obj[0].imgurl, obj[1].imgurl, obj[2].imgurl, obj[3].imgurl]
     context = {'sname': stu.sname, 'sroom': stu.sroom, 'scard': stu.scard, 'roommate': roommate,
-                'img_folder': img_folder, 'img_list': img_list,'sclass':stu.sclass}
+               'img_folder': img_folder, 'img_list': img_list, 'sclass': stu.sclass}
     return render(request, 'getID.html', context)
-
